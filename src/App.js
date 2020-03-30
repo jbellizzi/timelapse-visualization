b0ec4895d420a8aa2680be01b8d19dbc161470bf
John Bellizzi <jbellizzi@axisgroup.com>
2020-03-30 08:17:25 -0400

    useTimer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { useTimer } from "./hooks"
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { time, play, stop } = useTimer({
+		startTime: new Date("1/1/2020"),
+		endTime: new Date("4/30/2020"),
+		step: 1000 * 60 * 60,
+		frequency: 24,
+	})
+
+	return (
+		<div className="App">
+			<button onClick={play}>Play</button>
+			<button onClick={stop}>Stop</button>
+			<h1>{time.toLocaleString()}</h1>
+		</div>
+	)
 }
 
-export default App;
+export default App
